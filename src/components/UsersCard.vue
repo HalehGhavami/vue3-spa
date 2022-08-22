1f6271f8bc59efc64b84b10af821c30675ad5dae
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-08-22 11:23:21 +0430

    used boostrap Card component to display data passing through Props from parent component

---
+++ src/components/UsersCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ user.username }}</h5>
+      <p class="card-text">
+        {{ user.email }}
+      </p>
+      <li>{{ user.name }}</li>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line no-undef
+  name: 'UsersCard',
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style></style>
