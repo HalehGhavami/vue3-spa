696e5a9459e06a825bc4d04ee4d4318d80852d0a
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-08-22 11:17:09 +0430

    created Home component for displaying from element
    some custom styled added

---
+++ src/pages/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <form @submit.prevent="handleSubmit" class="row g-3 needs-validation">
+    <!-- novalidate -->
+    <div class="col-md-4">
+      <label for="validationCustom01" class="form-label">First name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="validationCustom01"
+        value="Haleh"
+        required
+      />
+      <div class="valid-feedback">Looks good!</div>
+    </div>
+    <div class="col-md-4">
+      <label for="validationCustom02" class="form-label">Last name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="validationCustom02"
+        value="Ghavami"
+        required
+      />
+      <div class="valid-feedback">Looks good!</div>
+
+      <button class="btn" type="submit">Submit form</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home',
+  data() {
+    return {
+      firstname: '',
+      lastname: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('submited');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  background: rgb(7, 24, 7);
+  border: 0;
+  padding: 10px 20px;
+  color: white;
+  border-radius: 20px;
+}
+</style>
