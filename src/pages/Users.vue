5558d79896d6e18a203d9a73e1ea04d4dde5b28e
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-08-22 11:24:28 +0430

    Create Users.vue

---
+++ src/pages/Users.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>User</h2>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Users',
+};
+</script>
+
+<style></style>
